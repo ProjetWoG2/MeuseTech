861b7605b9abbd07f33886605d6f7cfc1bf2d445
Jacques G <jcqs.gr@gmail.com>
2016-04-21 11:36:02 +0200

    21-04-16 Ajout des infos pour affichage 3 derniers projets etc...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,17 @@ class PagesController < ApplicationController
 	
 	
   def index 
+    @derniers_projets=Projet.order(created_at: :desc).first(3)
+    @projets=Projet.all
+    
+    @projets_actifs=Hash.new
+    
+    @projets.each do |projet|
+        score = ((projet.comments.where(role: "comments").where(visible:true).count)*6)+(projet.comments.where(role: "likes").count)
+        @projets_actifs[projet] = score
+    end
+    @projets_actifs = @projets_actifs.sort_by{|_key, value| value}.reverse.first(3)
+    @derniers_commentaires=Comment.last(5)
   end
     
   def pourquoi
